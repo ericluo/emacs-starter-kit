dd963a3e2220bc8a0feba192838fe57c7b4e28a6
eric <eric@Eric.(none)>
2010-03-19 14:19:22 +0800

    install the anything and anything-config library

--- custom.el
+++ custom.el
@@ -47,3 +47,15 @@
 
 (add-to-list 'load-path (concat dotfiles-dir "rinari"))
 (require 'rinari)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.32"))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
